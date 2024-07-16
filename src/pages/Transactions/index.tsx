874e57380f43c64a72a0014ecb882f31fcaa8880
Wellyson Martins <wellysonmartins@gmail.com>
2024-07-16 18:29:14 -0300

    add header, summary, searchform and modal

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,5 +1,6 @@
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
+import { SearchForm } from "./components/SerachForm";
 import {
   PriceHighlight,
   TransactionsContainer,
@@ -13,10 +14,12 @@ export function Transactions() {
       <Summary />
 
       <TransactionsContainer>
+        <SearchForm />
+
         <TransactionsTable>
           <tbody>
             <tr>
-              <td width="50%">Desenvolvimento de site</td>
+              <td width="40%">Desenvolvimento de site</td>
               <td>
                 <PriceHighlight variant="income">R$ 12.000,00</PriceHighlight>
               </td>
@@ -24,7 +27,7 @@ export function Transactions() {
               <td>13/04/2022</td>
             </tr>
             <tr>
-              <td width="50%">Hamburger</td>
+              <td width="40%">Hamburger</td>
               <td>
                 <PriceHighlight variant="outcome">- R$ 59,00</PriceHighlight>
               </td>
